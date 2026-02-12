480879782d4e29ed27672a5e4a0b5bb0b6581fbc
Wuliesiat <766296040@qq.com>
2026-02-12 11:24:04 +0800

    Create __init__.py

---
+++ chemistry_mcp/__init__.py
@@ -0,0 +1,9 @@
+"""
+Chemistry MCP Module
+
+This module provides tools for chemistry-related operations.
+"""
+
+from .pubchem_tools import search_advanced_pubchem
+
+__all__ = ["search_advanced_pubchem"]
