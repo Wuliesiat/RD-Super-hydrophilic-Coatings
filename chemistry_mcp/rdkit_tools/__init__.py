df60d5afc6f5de4206ea5e599b83883d967aca36
Wuliesiat <766296040@qq.com>
2026-02-12 11:25:05 +0800

    Create __init__.py

---
+++ chemistry_mcp/rdkit_tools/__init__.py
@@ -0,0 +1,15 @@
+"""
+RDKit Tools Package
+
+This package provides tools for molecular analysis, manipulation, and visualization
+using the RDKit library. It includes functions for calculating molecular descriptors,
+generating molecular fingerprints, analyzing molecular structures, and more.
+"""
+
+# Import from new module files
+from .rdkit_basic import *
+from .rdkit_extended import *
+from .rdkit_advanced import *
+from .rdkit_estate import *
+
+
