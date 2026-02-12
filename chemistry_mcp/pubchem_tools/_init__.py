141777989a07f51a60d2c076fab7bf2439aa953a
Wuliesiat <766296040@qq.com>
2026-02-12 11:24:36 +0800

    Create _init__.py

---
+++ chemistry_mcp/pubchem_tools/_init__.py
@@ -0,0 +1,9 @@
+"""
+PubChem Tools Module
+
+This module provides tools for accessing and processing chemical data from PubChem.
+"""
+
+from .pubchem_tools import search_advanced_pubchem
+
+__all__ = ["search_advanced_pubchem"]
